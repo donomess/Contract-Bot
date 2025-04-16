4e93f6a7a232fc8c5d87e81726b8571163a11ad6
donomess <donovanmess01@gmail.com>
2025-04-16 18:02:27 -0400

    fix pls dear god.

--- deploy-commands.js
+++ deploy-commands.js
@@ -89,7 +89,6 @@ const commands = [
         option.setName('evidence')
         .setDescription('Attachment Evidence of the termination (if needed)')
         .setRequired(false))
-
     .toJSON()
 ];
 
@@ -97,6 +96,12 @@ const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
 
 (async () => {
     try{
+        console.log("Clearing old commands...");
+        await rest.put(
+            Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID),
+            { body: [] }
+        )
+        console.log("Old commands cleared!");
         console.log("Reigstering commands...");
         await rest.put(
             Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID),
